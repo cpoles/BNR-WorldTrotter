62ff3f890ab3258a80c53fd8103c5b0626ff1c24
cpoles <carlos.poles@griffithuni.edu.au>
2017-11-07 23:07:38 +1000

    Added ConversionViewController custom class

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Carlos Poles on 7/11/17.
+//  Copyright © 2017 Carlos Poles. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController : UIViewController {
+    
+    
+    
+    
+    
+    
+}
+

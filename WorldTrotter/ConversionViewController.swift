22bf722b15eeed92ce424e043e110981dae0faae
cpoles <carlos.poles@griffithuni.edu.au>
2017-11-07 23:49:18 +1000

    Added computed properties for fahrenheit and celsius values. Added class method to update the celsius label

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -10,20 +10,46 @@ import UIKit
 
 class ConversionViewController : UIViewController {
     
+    // MARK: Outlets
+    
     // create outlet to the Celsius label and to the Text Field
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        // property observer to observe for changes so that
+        // whenever the value of this property changes, the observer is triggered
+        didSet {
+            updateCelsiusLabel() // call the updateCelsiusLabel() method
+                                // whenever the property is set
+        }
+    }
     
-    // Action Methods
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
     
-    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-        // check if text field contains any text
-        if let text = textField.text, !text.isEmpty {
-            celsiusLabel.text = text
+    // MARK: Class Methods
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = "\(celsiusValue)"
         } else {
             celsiusLabel.text = "???"
         }
+    }
+    
+    
+    
+    
+    // MARK: Action Methods
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        // check if text field contains any text and check as it changes
+        
     }
     
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
